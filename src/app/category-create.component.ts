fe5eb7797451f93e15034e4df4452bd372b99edf
Edvaldo Junior <ejunnior@gmail.comgit config --global user.email ejunnior@gmail.comgit config --global user.email ejunnior@gmail.com>
2021-07-07 13:42:04 +0100

    initial commit

---
+++ src/app/category-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'fm-categorycreate',
+    templateUrl: './category-create.component.html'
+})
+
+export class CategoryCreateComponent{
+    pageTitle: string =  "Category";
+}
